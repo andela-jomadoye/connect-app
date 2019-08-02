515632a8b09066cd7281374ac787bbe976c7dbd5
jomadoye <omadoyejed@gmail.com>
2019-08-02 19:11:39 +0100

    initial dev

--- config/constants/index.js
+++ config/constants/index.js
@@ -1,5 +1,5 @@
 module.exports = (() => {
-  const branch = process.env.CIRCLE_BRANCH || 'dev'
+  const branch = process.env.CIRCLE_BRANCH || 'dev';
 
   // for security reason don't let to require any arbitrary file defined in process.env
   if (['master', 'qa'].indexOf(branch) < 0) {
